6b470d47dc2102752afc488b409ad1fbfabd0293
Omar Guerrero <loberoxd.4@gmail.com>
2022-07-07 19:07:34 -0500

    first commit

---
+++ gateway-service/src/main/java/com/gateway/service/GatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
